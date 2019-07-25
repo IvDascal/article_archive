e483531f787d517e3e9b0127b5b7a6fbfdbabb40
Vano <dascal.iv@gmail.com>
2019-07-25 11:26:15 +0300

    add db config

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'article-archive-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'article_archive.db')
