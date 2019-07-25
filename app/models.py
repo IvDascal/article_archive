2dc2bb5e7715b854eca6d065ad6955dbf2d26d96
Vano <dascal.iv@gmail.com>
2019-07-25 18:02:23 +0300

    add user and role models

---
+++ app/models.py
@@ -0,0 +1,47 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role {}'.format(self.name)
+
+    def __str__(self):
+        return 'Role: {} --- {}'.format(self.id, self.name)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password_hash = db.Column(db.String(128))
+    is_active = db.Column(db.Boolean, default=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    # json field? - дополнительные сопутствующие поля если нужно
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def __str__(self):
+        return 'User: {} --- {}'.format(self.id, self.username)
