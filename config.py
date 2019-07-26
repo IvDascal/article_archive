4e518d53daa72bb2c9fc3ffa8991581750fc54fe
Vano <dascal.iv@gmail.com>
2019-07-26 12:37:27 +0300

    minor fix

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'article-archive-secret-key'
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'article_archive.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
