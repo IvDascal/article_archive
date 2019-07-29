0bc10fdb676c30a51af48af19b02b105463cdcca
Vano <dascal.iv@gmail.com>
2019-07-29 12:52:12 +0300

    add user CRUD

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
+
+from app.models import Role, User
 
 
 class LoginForm(FlaskForm):
@@ -8,3 +10,47 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class UserCreateForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    is_active = BooleanField('Active', default="checked")
+    role = SelectField('Role', coerce=int)
+    submit = SubmitField('Create')
+
+    def __init__(self, *args, **kwargs):
+        super(UserCreateForm, self).__init__(*args, **kwargs)
+        self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use.')
+
+
+class UserEditForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    is_active = BooleanField('Active', default="checked")
+    role = SelectField('Role', coerce=int)
+    submit = SubmitField('Save')
+
+    def __init__(self, user, *args, **kwargs):
+        super(UserEditForm, self).__init__(*args, **kwargs)
+
+        self.role.choices = [(role.id, role.name) for role in Role.query.order_by(Role.name).all()]
+        self.user = user
+
+    def validate_email(self, field):
+        if field.data != self.user.email and \
+                User.query.filter_by(email=field.data).first(): raise ValidationError('Email already registered.')
+
+    def validate_username(self, field):
+        if field.data != self.user.username and \
+                User.query.filter_by(username=field.data).first(): raise ValidationError('Username already in use.')
--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,9 @@ from flask import render_template, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user
 from werkzeug.urls import url_parse
 
-from app import app
-from app.forms import LoginForm
-from app.models import User
+from app import app, db
+from app.forms import LoginForm, UserCreateForm, UserEditForm
+from app.models import User, Role
 
 
 @app.route('/')
@@ -42,3 +42,76 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/user/<int:id>')
+def user(id):
+    user = User.query.get(id)
+
+    return render_template('user_detail.html', user=user)
+
+
+@app.route('/users')
+def users():
+    users = User.query.all()
+    print(users)
+    return render_template('users.html', users=users)
+
+
+@app.route('/create_user', methods=['GET', 'POST'])
+def create_user():
+    form = UserCreateForm()
+    if form.validate_on_submit():
+        user = User()
+        user.username = form.username.data
+        user.email = form.email.data
+        user.password = form.password.data
+        user.is_active = form.is_active.data
+        print(form.role.data)
+        role = Role.query.get(form.role.data)
+        user.role = role
+        print(user)
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('users'))
+
+    return render_template('create_user.html', form=form)
+
+
+@app.route('/edit_user/<int:id>', methods=['GET', 'POST'])
+def edit_user(id):
+    user = User.query.get(id)
+    form = UserEditForm(user=user)
+    print(user)
+    if form.validate_on_submit():
+        user.username = form.username.data
+        user.email = form.email.data
+        user.is_active = form.is_active.data
+        role = Role.query.get(form.role.data)
+        user.role = role
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('user', id=user.id))
+
+    form.email.data = user.email
+    form.username.data = user.username
+    form.role.data = user.role_id
+
+    return render_template('edit_user.html', form=form)
+
+
+@app.route('/delete_user/<int:id>')
+def delete_user(id):
+    user = User.query.get(id)
+    user.is_active = False
+
+    db.session.add(user)
+    db.session.commit()
+
+    # TODO delete confirmation to avoid misclick
+    # TODO success  or error message
+
+    return redirect(url_for('users'))
